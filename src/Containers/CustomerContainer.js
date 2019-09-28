43da0009632cddd2f38db0c77e7bb94bb638b7bc
Peter Viss <peter.c.viss@biola.edu>
2019-09-28 01:23:25 -0400

    testing

--- src/Containers/CustomerContainer.js
+++ src/Containers/CustomerContainer.js
@@ -106,6 +106,7 @@ class CustomerContainer extends Component {
   }
   //////////////////////////////////////////////////////////////////////////////////////////////////////
   chooseFarmer = farmer => {
+    console.log(farmer)
     if (farmer.followers.length !== 0) {
       let followers = farmer.followers.filter(follower => {
         if (follower.id === this.props.customer.id) {
